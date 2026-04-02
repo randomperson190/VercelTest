cee65904e85c429c894e59d9f00ba6d0f87a9caa
randomperson190 <53307937+randomperson190@users.noreply.github.com>
2026-04-02 20:00:04 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,15 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const data = await response.json();
+  try {
+    const response = await fetch("https://api.argentinadatos.com/v1/finanzas/indices/inflacion");
+    const data = await response.json();
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json(data);
+    // agarrar el último dato
+    const ultimo = data[data.length - 1];
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(ultimo);
+
+  } catch (error) {
+    res.status(500).json({ error: "Error obteniendo datos" });
+  }
 }
